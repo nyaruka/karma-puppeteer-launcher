9aa7fa6ac7edf5b5666970a554dd55f442bee874
Eric Newcomer <ericnewcomer@gmail.com>
2020-09-16 16:21:22 -0700

    tweak threshold

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ const compareScreenshots = async (paths, exluded) => {
 
       // Do the visual diff.
       const diff = new PNG({ width: img1.width, height: img2.height });
-      const matchOptions = { threshold: 0.2 };
+      const matchOptions = { threshold: 0.3 };
       const numDiffPixels =
         exluded != null && exluded.length != 0
           ? dynamicpixelmatch(
